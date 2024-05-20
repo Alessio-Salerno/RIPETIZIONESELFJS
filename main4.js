de59ace759eadc167217f64e1932279c0c89c36b
Alessio-Salerno <156314493+Alessio-Salerno@users.noreply.github.com>
2024-05-20 12:38:02 +0200

    Add files via upload

---
+++ main4.js
@@ -0,0 +1,13 @@
+let num1 = parseInt(prompt('Inserisci il primo numero'));
+let num2 = parseInt(prompt('Inserisci il secondo numero'));
+
+let somma = num1 + num2;
+let sottrazione = num1 - num2;
+let moltiplicazione = num1 * num2;
+let divisione = num1 / num2;
+let potenza = num1 ** num2 ;
+
+console.log('con i numeri scelti, i risultati delle varie operazioni sono ')
+
+console.log(`somma ${somma}, sottrazione ${sottrazione}, moltiplicazione ${moltiplicazione}, divisione ${divisione}, potenza ${potenza}`);
+
